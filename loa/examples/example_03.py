b35b1aa587dbfe203fc1591f71920f434da4cdad
wkdgnl123 <69183660+wkdgnl123@users.noreply.github.com>
2020-12-14 21:42:07 +0900

    as

--- loa/examples/example_03.py
+++ loa/examples/example_03.py
@@ -1,3 +1,4 @@
+import unittest
 from loa.unit import Unit
 from loa.team import Team
 from loa.team import TeamExaminer
@@ -40,7 +41,7 @@ class MyUnit2(Unit):
                          arm=cls.ARM,
                          evs=cls.EVS)
 
-class MyTeam1(Team):
+class MyTeam(Team):
     def initialize(self):
         for i in range(10):
             unit = MyUnit1(self, "A-Unit%02d"%(i+1), i)
@@ -69,37 +70,5 @@ class MyTeam2(Team):
 
     
 if __name__ == "__main__":
-   
-    use_logging("test",                
-                stdout=False,
-                fout=True,
-                fpath="test.log",
-                mode='w')
-    
-    simulator = BasicSimulator()
-    team1 = MyTeam1("Team#1")
-    team2 = MyTeam2("Team#2")
-    print(team1)
-    print()
-    print(team2)
-  
-    examiner = TeamExaminer()
-    examiner.check(team1, "ROUND-02")
-    examiner.check(team2, "ROUND-02")    
-
-    judge = EachTurnMaxSurvivalJudge()
-    n_team1, n_team2, n_draws = simulator.play(team1, team2, 20, 10, judge)
-    print("Number of Team1 wins:", n_team1)
-    print("Number of Team2 wins:", n_team2)
-    print("Number of draws:", n_draws)
-    print()
-    
-    if n_team1 > n_team2:
-        print("Team #1 wins!")
-    elif n_team1 < n_team2:
-        print("Team #2 wins!")        
-    else:
-        print("Two teams draw...")
-
-    finish_logging()
+    unittest.main()
         
